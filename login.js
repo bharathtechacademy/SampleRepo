0551662e4fb0529c2a3c3ff515ae9224e492a171
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-04-23 21:22:09 +0530

    Revert "added 5th line"
    This reverts commit 650b92c10345793c8a24e1905deef014205999ea.

--- login.js
+++ login.js
@@ -2,5 +2,4 @@ console.log("Login Page - Line 1");
 console.log("Login Page - Line 2");
 console.log("Login Page - Line 3 Added By Bharath");
 console.log("Login Page - Line 3 Added By Anurag");
-console.log("Login Page - Line 5 Added By Bharath");
 
